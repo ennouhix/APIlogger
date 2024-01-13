a6101d2bb468dbd8d964430ae6dacf065c0b97e9
ennouhix <127449312+ennouhix@users.noreply.github.com>
2024-01-13 19:45:37 +0100

    VF
    version final

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,9 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) {
+        //Enter the Api Full Url Here
+        HttpRequest.fetch("https://httpbin.org/cookies");
+
+    }
+}
